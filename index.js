a080e0d114d8bc4f128102e4617d9da95466dcfb
MouS0926 <moupriya2609@gmail.com>
2023-09-25 00:46:27 +0530

    test-modd

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const cors=require("cors")
 const { UserRouter } = require("./Routes/UserRouter")
 
 
+
 const app=express()
 
 app.use(express.json())
 app.use(cors())
 app.use("/user",UserRouter)
 
-app.get("/",(req,res)=>{
-    res.status(200).send("get req")
-})
+// app.get("/",(req,res)=>{
+//     res.status(200).send("get req")
+// })
 
 app.listen(8080,async ()=>{
     
